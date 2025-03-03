effe04064c456791ae4b43f90976f75f6db01381
berkaycelikten <berkayceliktenn@gmail.com>
2025-03-03 14:33:15 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BERKAY\\SQLEXPRESS;Initial Catalog=ApiAIDb;Integrated Security=True;Trust Server Certificate=True");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
