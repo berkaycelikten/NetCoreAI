186888ca5891e8a705900fe76b9a406e6213d1ed
berkaycelikten <berkayceliktenn@gmail.com>
2025-03-14 16:42:42 +0300

    speech to text and chat app with console

---
+++ NetCoreAI.Project3_RapidApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetCoreAI.Project3_RapidApi.ViewModels;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "996f1e32b5mshd186dd911bfb9aep16eeb0jsn136707b4918c" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonSerializer.Deserialize<List<ApiSeriesViewModel>>(body);
+    foreach (var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title + "-Film Puanı: " + series.rating + "-Yapım Yılı: " + series.year);
+    }
+}
+
+Console.ReadLine();
